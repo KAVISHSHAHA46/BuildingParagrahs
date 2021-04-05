c5567a1bf800fc1d215c1bc239e38d164c8b6247
KAVISHSHAHA46 <73390443+KAVISHSHAHA46@users.noreply.github.com>
2021-04-05 10:09:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+function getpara1(){
+    input1=document.getElementById("input_box_1").value;
+input2=document.getElementById("input_box_2").value;
+input3=document.getElementById("input_box_3").value;
+input4=document.getElementById("input_box_4").value;
+input5=document.getElementById("input_box_5").value;
+input6=document.getElementById("input_box_6").value;
+input7=document.getElementById("input_box_7").value;
+input8=document.getElementById("input_box_8").value;
+input9=document.getElementById("input_box_9").value;
+input10=document.getElementById("input_box_10").value;
+input11=document.getElementById("input_box_11").value;
+input12=document.getElementById("input_box_12").value;
+
+    document.getElementById("output").innerHTML=input1+input2+input3+input4+input5+input6+input7+input8+input9+input10+input11+input12;
+}
+
+function getpara2(){
+    input1=document.getElementById("input_box_1").value;
+input2=document.getElementById("input_box_2").value;
+input3=document.getElementById("input_box_3").value;
+input4=document.getElementById("input_box_4").value;
+input5=document.getElementById("input_box_5").value;
+input6=document.getElementById("input_box_6").value;
+input7=document.getElementById("input_box_7").value;
+input8=document.getElementById("input_box_8").value;
+input9=document.getElementById("input_box_9").value;
+input10=document.getElementById("input_box_10").value;
+input11=document.getElementById("input_box_11").value;
+input12=document.getElementById("input_box_12").value;
+
+    document.getElementById("output2").innerHTML=input1+input2+input3+input4+input5+input6+input7+input8+input9+input10+input11+input12;
+}
+
+
+
+
+
+
+
